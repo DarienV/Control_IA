98cbef2553ba795aafaa9d7f422fdb97154d4447
Darien Ramirez <didier.vital74@gmail.com>
2023-02-26 22:00:22 -0600

    Se agregan los documentos de preguntas y respuestas, se crea un diccionario de las respuestas correctas

--- Examen1/Cuestionario.py
+++ Examen1/Cuestionario.py
@@ -9,9 +9,14 @@ def randomquestion(ls):
     ls.remove(rq)
     return  rq
 
-q1 = randomquestion(lista)
-q2 = randomquestion(lista)
-q3 = randomquestion(lista)
+answers = {
+    1:"a", 2:"a", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"", 10:"",
+    11:"c", 12:"b", 13:"d", 14:"d", 15:"a", 16:"a", 17:"c", 18:"d", 19:"c", 20:"a",
+    21:"a", 22:"c", 23:"c", 24:"a", 25:"b", 26:"a", 27:"", 28:"", 29:"", 30:"",
+    31:"b", 32:"b", 33:"a", 34:"c", 35:"b", 36:"a", 37:"d", 38:"a", 39:"a", 40:"a",
+    41:"b", 42:"b", 43:"b", 44:"a", 45:"b", 46:"c", 47:"b", 48:"a", 49:"a", 50:"d"
+    }
 
-print(q1,q2,q3)
-print(lista)
+for _ in range(1,4):
+    q = randomquestion(lista)
+    
