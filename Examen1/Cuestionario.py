398c243a5328b35ad22b18c7868db0d4550bf78a
Darien Ramirez <didier.vital74@gmail.com>
2023-02-26 23:18:07 -0600

    Se imprimen preguntas y respuestas, se compara la respuesta del usuario con el diccionario para la calificacion final

--- Examen1/Cuestionario.py
+++ Examen1/Cuestionario.py
@@ -1,4 +1,13 @@
 import random
+import csv
+
+correct_answers = {
+    1:"a", 2:"a", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"", 10:"",
+    11:"c", 12:"b", 13:"d", 14:"d", 15:"a", 16:"a", 17:"c", 18:"d", 19:"c", 20:"a",
+    21:"a", 22:"c", 23:"c", 24:"a", 25:"b", 26:"a", 27:"", 28:"", 29:"", 30:"",
+    31:"b", 32:"b", 33:"a", 34:"c", 35:"b", 36:"a", 37:"d", 38:"a", 39:"a", 40:"a",
+    41:"b", 42:"b", 43:"b", 44:"a", 45:"b", 46:"c", 47:"b", 48:"a", 49:"a", 50:"d"
+    }
 
 lista = []
 for _ in range(1,51):
@@ -9,14 +18,23 @@ def randomquestion(ls):
     ls.remove(rq)
     return  rq
 
-answers = {
-    1:"a", 2:"a", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"", 10:"",
-    11:"c", 12:"b", 13:"d", 14:"d", 15:"a", 16:"a", 17:"c", 18:"d", 19:"c", 20:"a",
-    21:"a", 22:"c", 23:"c", 24:"a", 25:"b", 26:"a", 27:"", 28:"", 29:"", 30:"",
-    31:"b", 32:"b", 33:"a", 34:"c", 35:"b", 36:"a", 37:"d", 38:"a", 39:"a", 40:"a",
-    41:"b", 42:"b", 43:"b", 44:"a", 45:"b", 46:"c", 47:"b", 48:"a", 49:"a", 50:"d"
-    }
+with open("Preguntas.txt", "r", encoding="utf8") as q:
+    questions = q.readlines()
 
-for _ in range(1,4):
+with open("Respuestas.csv", "r", encoding="utf8") as a:
+    answers = a.readlines()
+
+score = 0
+
+for i in range(1,4):
     q = randomquestion(lista)
+    print(questions[q])
+    aux = answers[q].split(",")
+    for j in aux:
+        print(j)
     
+    user_answer = input("La respuesta es: ")
+    if user_answer.lower() == correct_answers[q]:
+        score += 1
+    
+print("\n\nObtuvo " + str(score) + "/3 de calificacion.")
