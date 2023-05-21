3b7055094c7f0717bdad9d36756e42720c5a8e23
Darien Ramirez <didier.vital74@gmail.com>
2023-05-21 11:21:49 -0600

    Codigo actualizado, 5 y n epocas

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -1,8 +1,25 @@
 import numpy as np
 
-w = 0.75
-Eta = 0.9
-def perceptron_training(X, y, learning_rate=Eta):
+def perceptron_training5(X, y, w, Eta):
+    num_features = X.shape[1]
+    weights = np.full(num_features, w)
+    epoch = 0
+
+    for _ in range(5):
+        
+        for i in range(len(X)):
+            u = np.dot(weights, X[i])
+            y_pred = np.sign(u)
+
+            if y_pred != y[i]:
+                weights += Eta * (y[i] - y_pred) * X[i]
+                error = True
+
+        epoch += 1
+
+    return weights, epoch
+
+def perceptron_training(X, y, w, Eta):
     num_features = X.shape[1]
     weights = np.full(num_features, w)
     epoch = 0
@@ -15,7 +32,7 @@ def perceptron_training(X, y, learning_rate=Eta):
             y_pred = np.sign(u)
 
             if y_pred != y[i]:
-                weights += learning_rate * (y[i] - y_pred) * X[i]
+                weights += Eta * (y[i] - y_pred) * X[i]
                 error = True
 
         epoch += 1
@@ -27,21 +44,27 @@ def perceptron_training(X, y, learning_rate=Eta):
 # Example usage
 X = np.array([[-1, 0.1, 0.4, 0.7], [-1, 0.3, 0.7, 0.2], [-1, 0.6, 0.9, 0.8], [-1, 0.5, 0.7, 0.1]])
 y = np.array([1, -1, -1, 1])
-
-weights, epochs = perceptron_training(X, y)
-print("Weights:", weights)
-print("Number of epochs:", epochs)
+w = [0.1, 0.75]
+Eta = [0.5, 0.9]
 
 save_path = "C:\\Users\\didie\\Documents\\BUAP_Prim23\\Control_IA\\Proyecto\\"
-outfile = open(save_path + "Perceptron_w({})_Eta({}).csv".format(str(w),str(Eta)), "w")
-outfile.write("# of Execution,Weights,Epochs\n")
+outfile = open(save_path + "Perceptron_outdata.csv", "w")
+outfile.write("w,Eta,Weights,Epochs\n")
 
-execution = []
-for i in range(5):
-    weights, epochs = perceptron_training(X, y)
-    print("Weights:", weights)
-    print("Number of epochs:", epochs)
-    execution.append(i)
-    outfile.write("{},{},{}\n".format(str(execution[i]), str(weights), str(epochs)))
+for n in w:
+    for m in Eta:
+        weights5, epochs5 = perceptron_training5(X, y, n, m)
+        print("Weights:", weights5)
+        print("Number of epochs:", epochs5)
+        outfile.write("{},{},{},{}\n".format(str(n), str(m), str(weights5), str(epochs5)))
+
+print("\n")
+        
+for n in w:
+    for m in Eta:
+        weights, epochs = perceptron_training(X, y, n, m)
+        print("Weights:", weights)
+        print("Number of epochs:", epochs)
+        outfile.write("{},{},{},{}\n".format(str(n), str(m), str(weights), str(epochs)))
 
 outfile.close()
